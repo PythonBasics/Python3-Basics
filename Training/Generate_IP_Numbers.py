efc0001906539878b99157c8f385ef9d63f77467
Download Brave <40052027+DownloadBrave@users.noreply.github.com>
2018-11-04 19:17:28 +0200

    New

--- Training/Generate_IP_Numbers.py
+++ Training/Generate_IP_Numbers.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from random import randint
+
+iplist= open("iplist.txt","w+")
+ip = []
+for i in range(0,10000):
+    ip.append("10.10." + str(randint(0,255)) + "." + str(randint(0,255)))
+for i in range(0,len(ip)):
+    iplist.write(str(ip[i]+"\n"))
+iplist.close()
