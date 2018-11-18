2d1770898c59dce5e3dabd1831539dfecd29f9e7
Download Brave <40052027+DownloadBrave@users.noreply.github.com>
2018-11-18 21:11:10 +0200

    new

--- Training/Visual_Studio_test.py
+++ Training/Visual_Studio_test.py
@@ -1,1 +1,7 @@
-print("Hello Worlds")
+try:
+    number = int(input("Enter number: "))
+    print (number)
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+except ValueError:
+    print("invalid value")
